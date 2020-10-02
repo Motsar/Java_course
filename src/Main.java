af659f37e7b048e8a9ee6a66afda1aa249da5a83
martin.motsar <martin.motsar@khk.ee>
2020-10-02 13:28:19 +0300

    Ül.14 - Number on positiivne või negatiivne

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Type in the radius:");
-        int radius = Integer.parseInt(reader.nextLine());
-        double circumreference = (double)radius * 2 * Math.PI;
-        System.out.print("Circumference of the circle: " + circumreference);
+        System.out.print("Type in a number:");
+        int number = Integer.parseInt(reader.nextLine());
+        if (number >= 0) {
+            System.out.println("The number is positive!");
+        } else {
+            System.out.println("The number is negative!");
+        }
     }
 }
