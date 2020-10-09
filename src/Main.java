af4add4a05f00a309e38fc8c0f09c2dd5b3bfbff
martin.motsar <martin.motsar@khk.ee>
2020-10-09 09:49:22 +0300

    Ül.19 - Vanuse võimalikuse kontroll

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Type in a number:");
-        int points = Integer.parseInt(reader.nextLine());
-
-        if (points < 0 || 60 < points) {
-            System.out.println("Points need to be between 0 and 60");
+        System.out.print("Type in your age:");
+        int age = Integer.parseInt(reader.nextLine());
+        String status ="";
+        if (age > 0 && 120 > age) {
+            status = "OK";
         } else {
-            if (points >= 0 && points <= 29) {
-                String grade = "failed";
-                System.out.println("you have " + grade + " the test!");
-            } else {
-                int grade = 0;
-                if (points > 29 && points <= 34) {
-                    grade = 1;
-                } else if (points > 35 && points <= 39) {
-                    grade = 2;
-                } else if (points > 40 && points <= 44) {
-                    grade = 3;
-                } else if (points > 45 && points <= 49) {
-                    grade = 4;
-                } else if (points > 50 && points <= 60) {
-                    grade = 5;
-                }
-                System.out.println("Grade is :"+ grade +" !");
-            }
+            status = "Impossible!";
         }
+        System.out.println(status);
     }
 }
