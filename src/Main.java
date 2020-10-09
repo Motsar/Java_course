01068d3226705f09d871bb39fdbb18f2f2333ee8
martin.motsar <martin.motsar@khk.ee>
2020-10-09 09:39:44 +0300

    Ül.18 - Hinde määramise ülesanne

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,29 @@ public class Main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         System.out.print("Type in a number:");
-        int number = Integer.parseInt(reader.nextLine());
-        if (number % 2 == 1) {
-            System.out.println("The number is odd!");
+        int points = Integer.parseInt(reader.nextLine());
+
+        if (points < 0 || 60 < points) {
+            System.out.println("Points need to be between 0 and 60");
         } else {
-            System.out.println("The number is even!");
+            if (points >= 0 && points <= 29) {
+                String grade = "failed";
+                System.out.println("you have " + grade + " the test!");
+            } else {
+                int grade = 0;
+                if (points > 29 && points <= 34) {
+                    grade = 1;
+                } else if (points > 35 && points <= 39) {
+                    grade = 2;
+                } else if (points > 40 && points <= 44) {
+                    grade = 3;
+                } else if (points > 45 && points <= 49) {
+                    grade = 4;
+                } else if (points > 50 && points <= 60) {
+                    grade = 5;
+                }
+                System.out.println("Grade is :"+ grade +" !");
+            }
         }
     }
 }
