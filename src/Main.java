0736145c2489b92fa731e24e7769ceaca9d4bbeb
martin.motsar <martin.motsar@khk.ee>
2020-10-15 06:32:10 +0300

    ÜL.36 summa,numbrite loendus, paaris ja paaritud arvud, keskmine sisestatud arv

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,31 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Until what? ");
-        int higherNumber = Integer.parseInt(reader.nextLine());
-        int currentNumber = 1;
+        System.out.println("Type numbers: ");
         int sum = 0;
-        while (currentNumber <= higherNumber){
-            sum += currentNumber;
-            currentNumber++;
+        int countAll = 0;
+        int countEven = 0;
+        int countOdd = 0;
+        double average = 0.0;
+        while(true){
+            int number = Integer.parseInt(reader.nextLine());
+            if(number == -1){
+                break;
+            }
+            countAll++;
+            if(number % 2 == 0){
+                countEven++;
+            } else {
+                countOdd++;
+            }
+            sum += number;
+            average = (double)sum / countAll;
         }
-        System.out.println("Sum is :" + sum);
+        System.out.println("Thank you and see you later!");
+        System.out.println("Sum is " + sum);
+        System.out.println("How many numbers: " + countAll);
+        System.out.println("How many even numbers: " + countEven);
+        System.out.println("How many odd numbers: " + countOdd);
+        System.out.println("Average: " + average);
     }
 }
