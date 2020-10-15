1a7b13a31f8e6f187538c95cc470b50f7b255465
martin.motsar <martin.motsar@khk.ee>
2020-10-15 07:13:07 +0300

    ÜL.56  Teksti tagurpidi välja printimine

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Type a word: ");
-        String word = input.nextLine();
-        System.out.print("Length of the first part: ");
-        int lengthOfFirstPart = Integer.parseInt(input.nextLine());
-        System.out.println("Result: " + word.substring(0, lengthOfFirstPart));
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Type in your text: ");
+        String text = reader.nextLine();
+        System.out.println("In reverse order: " + reverseText(text));
+    }
+
+    public static String reverseText(String text) {
+        // write your code here
+        String reverseText = "";
+        int i = text.length() - 1;
+        while (i >= 0) {
+            char symbol = text.charAt(i);
+            reverseText += symbol;
+            i--;
+        }
+        return reverseText;
     }
 }
