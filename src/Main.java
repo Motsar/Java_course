6b951002d1741db2cda90325a0bcb4e801dfe264
martin.motsar <martin.motsar@khk.ee>
2020-10-15 11:15:52 +0300

    Ül.69 Kas sisestatud tekst on tagurpidi lugedes võrdväärne

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,33 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Scanner;
 
 
 public class Main {
 
-    public static int greatest(ArrayList<Integer> list) {
+    public static boolean palindrome(String text) {
         // write your code here
-        Collections.sort(list);
-        int count = 0;
-
-        for(int listItem: list){
-            count+=1;
+        String reverseText = "";
+        int i = text.length() - 1;
+        while (i >= 0) {
+            char symbol = text.charAt(i);
+            reverseText += symbol;
+            i--;
         }
-
-        return list.get(count-1);
+        if(text!=reverseText){
+            return false;
+        }else{
+            return true;
+        }
     }
 
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        list.add(3);
-        list.add(2);
-        list.add(7);
-        list.add(22);
-        System.out.println("The greatest number is: " + greatest(list));
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Type a text: ");
+        String text = reader.nextLine();
+        if (palindrome(text)) {
+            System.out.println("The text is a palindrome!");
+        } else {
+            System.out.println("The text is not a palindrome!");
+        }
     }
 }
