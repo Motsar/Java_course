d3f8c1544711282ea1887035c34d941b8436628a
martin.motsar <martin.motsar@khk.ee>
2020-10-15 06:03:59 +0300

    Ül.21 On/ei ole liigaasta

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,21 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        System.out.print("Type in your age:");
-        int age = Integer.parseInt(reader.nextLine());
-        String status ="";
-        if (age > 0 && 120 > age) {
-            status = "OK";
-        } else {
-            status = "Impossible!";
+        Scanner input = new Scanner(System.in);
+        System.out.print("Type a year: ");
+        int year = Integer.parseInt(input.nextLine());
+        boolean leapyear = false;
+        if(year % 100 == 0){
+            if(year % 400 == 0){
+                leapyear = true;
+            }
+        } else if (year % 4 == 0){
+            leapyear = true;
         }
-        System.out.println(status);
+        if(leapyear){
+            System.out.println("The year is a leap year.");
+        } else {
+            System.out.println("The year is not a leap year.");
+        }
     }
 }
