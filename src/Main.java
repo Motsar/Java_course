6a9f7197c4fcaf6f2345232007b2a138131c1529
martin.motsar <martin.motsar@khk.ee>
2020-10-15 06:27:00 +0300

    ÜL.32 kõrgeima numbrini loenduse kõikide numbrite summa

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Up to what number? ");
-        int upperNumber = Integer.parseInt(reader.nextLine());
+        System.out.print("Until what? ");
+        int higherNumber = Integer.parseInt(reader.nextLine());
         int currentNumber = 1;
-        while (currentNumber <= upperNumber){
-            System.out.println(currentNumber);
+        int sum = 0;
+        while (currentNumber <= higherNumber){
+            sum += currentNumber;
             currentNumber++;
         }
+        System.out.println("Sum is :" + sum);
     }
 }
