7cdffae13ca876dd9fa40394414825f3f1c930d7
martin.motsar <martin.motsar@khk.ee>
2020-10-15 07:07:07 +0300

    ÜL.53  Ette antud stringi ja ette antud numbriga tähtede printimine

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Type your name: ");
-        String name = input.nextLine();
-        int i = 0;
-        if(name.length() >= 3){
-            while(i < 3){
-                System.out.println((i+1) + ". character: " + name.charAt(i));
-                i++;
-            }
-        }
+        System.out.print("Type a word: ");
+        String word = input.nextLine();
+        System.out.print("Length of the first part: ");
+        int lengthOfFirstPart = Integer.parseInt(input.nextLine());
+        System.out.println("Result: " + word.substring(0, lengthOfFirstPart));
     }
 }
