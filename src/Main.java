cece19733d77ac58636542bd7f1faba2d9ee20b1
martin.motsar <martin.motsar@khk.ee>
2020-10-15 06:55:49 +0300

    ÜL.46 nelja numbri keskmine

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,15 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int least(int number1, int number2) {
-        // write program code here
-        // do not print anything inside the method
-        int least;
-        if (number1 < number2) {
-            least = number1;
-        } else if (number2 < number1) {
-            least = number2;
-        } else {
-            least = number1;
-        }
-        // method needs a return in the end
-        return least;
+    public static double average(int number1, int number2, int number3, int number4) {
+        // write your code here
+        int sum = number1 + number2 + number3 + number4;
+        double averageNum = (double) sum / 4;
+        return averageNum;
     }
 
     public static void main(String[] args) {
-        int answer = least(2, 7);
-        System.out.println("Least: " + answer);
+        double answer = average(4, 112, 6, 1);
+        System.out.println("average: " + answer);
     }
 }
