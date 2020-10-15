a1c2076300c5744c7f260ea15e64986a904f82cd
martin.motsar <martin.motsar@khk.ee>
2020-10-15 06:39:40 +0300

    ÜL.39 tähekeste printimine

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args) {
-        printText();
+    private static void printStars(int amount) {
+        // you can print one star with the command
+        // System.out.print("*");
+        // call this command amount times
+        int i = 1;
+        while (i <= amount){
+            System.out.print("*");
+            i++;
+        }
+        System.out.println();
     }
 
-    public static void printText() {
-        // write your code here
-        System.out.println("See suvaline tekst on prinditud printText meetodiga.");
+    public static void main(String[] args) {
+        printStars(5);
+        printStars(3);
+        printStars(9);
     }
 }
