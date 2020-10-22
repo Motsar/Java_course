7b30ef05ecba14b551cfdf70179b9714af365812
martin.motsar <martin.motsar@khk.ee>
2020-10-22 17:48:47 +0300

    Ül. 77.2 LyyraCard. payEconomical & payGourmet

--- src/LyyraCard.java
+++ src/LyyraCard.java
@@ -5,7 +5,16 @@ public class LyyraCard {
         this.balance = balanceAtStart;
     }
 
+    public void payEconomical() {
+        this.balance -= 2.50;
+    }
+
+    public void payGourmet() {
+        this.balance -= 4.00;
+    }
+
     public String toString() {
         return "The card has " + this.balance + " euros";
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,12 @@ public class Main {
     public static void main(String[] args) {
         LyyraCard card = new LyyraCard(150);
         System.out.println(card);
+
+        card.payEconomical();
+        System.out.println(card);
+
+        card.payGourmet();
+        card.payEconomical();
+        System.out.println(card);
     }
 }
