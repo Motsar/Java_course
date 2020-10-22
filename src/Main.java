d5efe918713c6546a7ae8d181ded4ddf5c1918f0
martin.motsar <martin.motsar@khk.ee>
2020-10-22 17:44:49 +0300

    Ül. 77.1 LyyraCard

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,6 @@
-import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
-        Menu menulist = new Menu();
-        Scanner reader = new Scanner(System.in);
-        System.out.print("Type in a new meal for menu:");
-        String menuItem = reader.nextLine();
-        menulist.addMeal(menuItem);
-        while(menuItem.length()!=0){
-            System.out.print("Type in a new meal for menu:");
-            menuItem = reader.nextLine();
-            menulist.addMeal(menuItem);
-        }
-        System.out.println("Here is your menu Sir!");
-        menulist.printMeals();
-
-        menulist.clearMenu();
-
-        menulist.printMeals();
+        LyyraCard card = new LyyraCard(150);
+        System.out.println(card);
     }
 }
