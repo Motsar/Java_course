a0d50dc236fcf8978bddb4a848b7637c00275c68
martin.motsar <martin.motsar@khk.ee>
2020-10-25 14:19:03 +0200

    Ül. 79.3: Asking for numbers from the user

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         NumberStatistics stats = new NumberStatistics();
-        stats.addNumber(3);
-        stats.addNumber(5);
-        stats.addNumber(1);
-        stats.addNumber(2);
+        Scanner reader = new Scanner(System.in);
+        int insertedNum;
+        System.out.print("Type numbers: ");
+        insertedNum = Integer.parseInt(reader.nextLine());
+        while (insertedNum>=0){
+            stats.addNumber(insertedNum);
+            insertedNum = Integer.parseInt(reader.nextLine());
+        }
         System.out.println("Amount: " + stats.amountOfNumbers());
         System.out.println("sum: " + stats.sum());
         System.out.println("average: " + stats.average());
