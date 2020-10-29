065b8a71058f2bcf3f88274121b055dbf0bfc14c
martin.motsar <martin.motsar@khk.ee>
2020-10-29 06:16:35 +0200

    Ül. 87.1: Class Student

---
+++ src/Student.java
@@ -0,0 +1,20 @@
+public class Student {
+    private String name;
+    private String studentNumber;
+
+    public Student(String newName, String NewStudentNumber){
+        this.name= newName;
+        this.studentNumber=NewStudentNumber;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+
+    public String toString() {
+        return name + ' ' + '(' + studentNumber +')';
+    }
+}
