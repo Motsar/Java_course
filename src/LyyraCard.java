e5a907a954789dfaf023cdff493cbbc7b6de4aa8
martin.motsar <martin.motsar@khk.ee>
2020-10-29 04:52:08 +0200

    Ül. 86.1: The "stupid" Lyyra card

--- src/LyyraCard.java
+++ src/LyyraCard.java
@@ -1,34 +1,27 @@
 public class LyyraCard {
     private double balance;
 
-    public LyyraCard(double balanceAtStart) {
-        this.balance = balanceAtStart;
+    public LyyraCard(double balance) {
+        this.balance = balance;
     }
 
-    public void payEconomical() {
-        if(this.balance>=2.50){
-            this.balance -= 2.50;
-        }
-    }
-
-    public void payGourmet() {
-        if(this.balance>=4.00){
-            this.balance -= 4.00;
-        }
+    public double balance() {
+        return this.balance;
     }
 
     public void loadMoney(double amount) {
-        if(this.balance+amount>150){
-            this.balance = 150;
-        }else if(amount<0){
-            this.balance+=0;
-        }else{
-            this.balance += amount;
-        }
+        this.balance += amount;
     }
 
-    public String toString() {
-        return "The card has " + this.balance + " euros";
+    public boolean pay(double amount){
+        // the method checks if the balance of the card is at least the amount given as parameter
+        // if not, the method returns false meaning that the card could not be used for the payment
+        // if the balance is enough, the given amount is taken from the balance and true is returned
+        if(amount<=this.balance){
+            this.balance-=amount;
+            return true;
+        }
+        return false;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,14 @@
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        LyyraCard cardOfPekka = new LyyraCard(10);
 
-        Counter counter1 = new Counter(0, true);
-        Counter counter2 = new Counter(0);
-        Counter counter3 = new Counter(true);
-        Counter counter4 = new Counter();
+        System.out.println("money on the card " + cardOfPekka.balance() );
+        boolean succeeded = cardOfPekka.pay(8);
+        System.out.println("money taken: " + succeeded );
+        System.out.println("money on the card " + cardOfPekka.balance() );
 
-        counter1.increase();
-        counter2.increase();
-        counter3.increase();
-        counter4.increase();
-
-        counter1.decrease();
-        counter2.decrease();
-        counter3.decrease();
-        counter4.decrease();
-
-        counter1.decrease();
-        counter2.decrease();
-        counter3.decrease();
-        counter4.decrease();
-
-        counter1.decrease(10);
-        counter2.decrease(10);
-        counter3.decrease(10);
-        counter4.decrease(10);
-
-        System.out.println(counter1);
-        System.out.println(counter2);
-        System.out.println(counter3);
-        System.out.println(counter4);
+        succeeded = cardOfPekka.pay(4);
+        System.out.println("money taken: " + succeeded );
+        System.out.println("money on the card " + cardOfPekka.balance() );
     }
 }
