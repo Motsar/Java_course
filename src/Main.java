0240e04021621261e297d4d6106c66d4a7366b44
martin.motsar <martin.motsar@khk.ee>
2020-11-11 15:01:33 +0200

    Ül 100.1

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,8 @@ import java.util.Arrays;
 
 public class Main{
     public static void main(String[] args) {
-        int[] original = {1, 2, 3, 4};
-        int[] reverse = reverseCopy(original);
-
-        // print both
-        System.out.println( "original: " +Arrays.toString(original));
-        System.out.println( "reversed: " +Arrays.toString(reverse));
+        NightSky NightSky = new NightSky(0.1, 40, 10);
+        NightSky.printLine();
     }
-
-
-    public static int[] reverseCopy(int[] array) {
-        int[] newArray = new int[array.length];
-        int rI = array.length-1;
-        for (int i = 0; i<array.length;i++) {
-            newArray[i] = array[rI];
-            rI -=1;
-        }
-        return newArray;
-    }
-
 }
 
