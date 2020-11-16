ef8d1aa92d02e02913e8839f8b7ad3474a9da232
martin.motsar <martin.motsar@khk.ee>
2020-11-16 13:47:17 +0200

    WEEK 8 ÜL 12.4

---
+++ src/Purchase.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Purchase {
+    private String product;
+    private int amount;
+    private int unitPrice;
+
+    public Purchase(String product, int amount, int unitPrice){
+        this.product = product;
+        this.amount = amount;
+        this.unitPrice = unitPrice;
+    }
+
+    public int price(){
+        return this.amount*this.unitPrice;
+    }
+
+    public void increaseAmount(){
+        this.amount+=1;
+    }
+
+    public String toString(){
+        return product+ ": " +amount;
+    }
+}
