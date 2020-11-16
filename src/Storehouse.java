5468df774373c503ac031e5bc05e16d90ec9442d
martin.motsar <martin.motsar@khk.ee>
2020-11-16 13:05:51 +0200

    WEEK 8 ÜL 12.2

--- src/Storehouse.java
+++ src/Storehouse.java
@@ -1,16 +1,23 @@
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Storehouse {
-    Map<String, Integer> storedItemPrice;
+    private Map<String, Integer> storedItemPrice;
+    private Map<String, Integer> storedItemStock;
+
 
 
     public  Storehouse(){
         this.storedItemPrice = new HashMap<String, Integer>() {};
+        this.storedItemStock = new HashMap<String, Integer>() {};
+
     }
 
     public void addProduct(String product, int price, int stock){
         this.storedItemPrice.put(product,price);
+        this.storedItemStock.put(product,stock);
     }
 
     public int price(String product){
@@ -19,4 +26,24 @@ public class Storehouse {
         }
         return -99;
     }
+
+    public int stock(String product){
+        if(storedItemStock.containsKey(product)){
+            return storedItemStock.get(product);
+        }
+        return 0;
+    }
+
+    public boolean take(String product){
+        if(storedItemStock.containsKey(product)){
+            if(storedItemStock.get(product)>0){
+                int newStock = storedItemStock.get(product)-1;
+                this.storedItemStock.put(product,newStock);
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
 }
