de99dba8851fce2e556a1935affddf9dac16a52f
martin.motsar <martin.motsar@khk.ee>
2020-11-30 18:04:20 +0200

    WEEK 9 ÜL 18.3

--- src/moving/Main.java
+++ src/moving/Main.java
@@ -1,23 +1,20 @@
 package moving;
 
 
+import moving.domain.Box;
 import moving.domain.Item;
 import moving.domain.Thing;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<Item> items = new ArrayList<Item>();
-        items.add(new Item("passport", 2));
-        items.add(new Item("printer", 50));
-        items.add(new Item("toothbrash", 1));
-        items.add(new Item("circular saw", 100));
+        Box movingBox =new Box(200);
+        movingBox.addThing(new Item("passport", 2));
+        movingBox.addThing(new Item("printer", 50));
+        movingBox.addThing(new Item("toothbrash", 1));
+        movingBox.addThing(new Item("circular saw", 100));
 
-        Collections.sort(items);
-        System.out.println(items);
+        System.out.println(movingBox.getVolume());
     }
 }
---
+++ src/moving/domain/Box.java
@@ -0,0 +1,33 @@
+package moving.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box implements Thing{
+    private int maximumCapacity;
+    private List<Thing> items;
+    private int totalVolume;
+
+
+    public Box(int maximumCapacity){
+        this.maximumCapacity=maximumCapacity;
+        this.items = new ArrayList<>();
+    }
+
+    public boolean addThing(Thing thing){
+        if(thing.getVolume()+ getVolume()<=this.maximumCapacity){
+            this.items.add(thing);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int getVolume() {
+        totalVolume = 0;
+        for(Thing thing : this.items) {
+            totalVolume+=thing.getVolume();
+        }
+        return totalVolume;
+    }
+}
