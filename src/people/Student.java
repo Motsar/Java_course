c7251b9ca75fd3a984974b9bf257474d3725672a
martin.motsar <martin.motsar@khk.ee>
2020-12-01 03:04:48 +0200

    WEEK 10 ÜL 29.2

---
+++ src/people/Student.java
@@ -0,0 +1,24 @@
+package people;
+
+
+public class Student extends Person{
+
+    private int credit;
+
+    public Student(String name, String address){
+        super(name, address);
+        this.credit = 0;
+    }
+
+    public void study(){
+        this.credit++;
+    }
+
+    public int credits(){
+        return this.credit;
+    }
+
+
+
+
+}
--- src/people/main.java
+++ src/people/main.java
@@ -4,10 +4,11 @@ package people;
 
 public class main {
 
-    public static void main(String[] args){
-        Person pekka = new Person("Pekka Mikkola", "Korsontie Street 1 03100 Vantaa");
-        Person esko = new Person("Esko Ukkonen", "Mannerheimintie Street 15 00100 Helsinki");
-        System.out.println(pekka);
-        System.out.println(esko);
+    public static void main(String[] args) {
+        Student olli = new Student("Olli", "Ida Albergintie Street 1 00400 Helsinki");
+        System.out.println(olli);
+        System.out.println("credits " + olli.credits());
+        olli.study();
+        System.out.println("credits "+ olli.credits());
     }
 }
